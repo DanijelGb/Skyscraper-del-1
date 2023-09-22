5f0a06769b1a92c871a13459123c3a9ace5505db
The-Dagba <danijel.grbic1@hotmail.com>
2023-09-22 11:15:10 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+var index = 1;
+
+while (index != 118)
+{
+    if (index % 10 == 4)
+    {
+        Console.WriteLine(index + 1);
+        index = index + 2;
+    }
+    else if (index % 100 == 13)
+    {
+        Console.WriteLine(index + 2);
+        index = index + 3;
+    }
+    else
+    {
+        Console.WriteLine(index);
+        index = index + 1;
+    }
+}
